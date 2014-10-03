0ff2f47345a583438650b89d6f7f58189b5a12f9
shiyin88 <shiyin88@gmail.com>
2014-10-03 19:37:01 -0400

    10/3

--- Amazon/isRotated.java
+++ Amazon/isRotated.java
@@ -1,12 +1,27 @@
+/**
+Assume you have a method isSubstring which checks if
+one word is a substring of another.
+Given two strings, s1 and s2, write code to check
+if s2 is a rotation of s1 using only one call to isSubstring
+(e.g. "waterbottle" is a rotation of "erbottlewat")*/
+
 public boolean isRotation(String s1, String s2){
-  int len2 = s2.length();
-  if (len2 == s1.length() && len2 > 0){
-      String s1s1 = s1 + s1;
-      return isSubstring(s1s1, s2);
-  }
+    if (s1 == null || s2 == null){
+        return false;
+    }
+    int len2 = s2.length();
+    if (len2 == s1.length() && len2 > 0){
+        String s1s1 = s1 + s1;
+        return isSubstring(s1s1, s2);
+    }
 
 }
 
-public String noVowels(String word){
+public static boolean isSubstring(String str1, String str2){
+    if (str1.indexOf(str2) == -1){
 
+      return false;
+
+    }
+    return true;
 }
